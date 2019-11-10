7b4bb7c788e85503af46913ffc01a38799bd19c0
Timothy Cyrus <tcyrus@tcyr.us>
2019-11-10 16:26:11 -0500

    Added Base Role Management Code

--- cogs/role_management.py
+++ cogs/role_management.py
@@ -2,6 +2,8 @@ import discord
 from discord import Colour
 from discord.ext import commands
 
+import functools
+import operator
 
 class RoleManagement(commands.Cog):
     def __init__(self, bot):
@@ -30,11 +32,35 @@ class RoleManagement(commands.Cog):
             await ctx.send(f'Updated color for "{highest_role.name}"!')
 
     @commands.command()
-    async def appoint(self, ctx):
-        '''Appoint User to Role (WIP)'''
+    async def appoint(self, ctx, role_str: str, target_user: int):
+        '''Appoint User to a Role'''
         server = self.bot.get_guild(self.bot.config['server'])
-        print([(i.name, i.id) for i in server.roles])
-        await ctx.send("WIP Command!")
+        user = server.get_member(ctx.message.author.id)
+        appoint_roles = self.bot.config['appoint_roles']
+
+        new_role = next(filter(lambda r: r.name == role_str, server.roles), None)
+        if new_role is None:
+            await ctx.send(f'Invalid Role "{role_str}"!')
+            return
+
+        target_member = server.get_member(target_user)
+        if target_member is None:
+            await ctx.send(f'Can\'t find user with id "{target_user}"!')
+            return
+
+        is_admin = any(map(lambda r: r.permissions.administrator, user.roles))
+
+        if not is_admin:
+            appointable_roles = map(lambda r: appoint_roles.get(r.name, []), user.roles)
+            appointable_roles = functools.reduce(operator.iconcat, appointable_roles, [])
+
+            if not any(map(lambda r: r.id == new_role.id, appointable_roles)):
+                await ctx.send(f'You are not authorized to appoint role "{role}"!')
+                return
+
+        await target_member.add_roles(new_role)
+
+        await ctx.send("Success")
 
 
 def setup(bot):
